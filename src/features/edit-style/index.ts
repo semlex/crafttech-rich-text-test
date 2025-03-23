15653a986beee2f8b170e21787ffb003af53f67a
semlex <picasso.semenov.spb@gmail.com>
2025-03-23 07:41:38 +0300

    add edit style feature

---
+++ src/features/edit-style/index.ts
@@ -0,0 +1,1 @@
+export { default as EditStyle } from './ui/edit-style';
---
+++ src/features/edit-style/ui/edit-style.tsx
@@ -0,0 +1,54 @@
+import { updateSelectedFigure } from '@/entities/figure';
+import { useAppDispatch, useAppSelector } from '@/shared/libs';
+import { useCallback } from 'react';
+import styles from './edit-style.module.scss';
+
+const EditStyle = () => {
+  const dispatch = useAppDispatch();
+  const selectedFigure = useAppSelector((state) => state.figure.selectedFigure);
+
+  const handleInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newValue = e.target.value;
+      const field = e.target.name;
+
+      if (selectedFigure) {
+        dispatch(
+          updateSelectedFigure({
+            [field]: newValue,
+          }),
+        );
+      }
+    },
+    [dispatch, selectedFigure],
+  );
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.inputGroup}>
+        <input
+          className={styles.colorPicker}
+          type="color"
+          id="fill"
+          name="fill"
+          value={selectedFigure?.fill}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="head">Цвет заливки</label>
+      </div>
+      <div className={styles.inputGroup}>
+        <input
+          className={styles.colorPicker}
+          type="color"
+          id="stroke"
+          name="stroke"
+          value={selectedFigure?.stroke}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="stroke">Цвет границы</label>
+      </div>
+    </div>
+  );
+};
+
+export default EditStyle;
