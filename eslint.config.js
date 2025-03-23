571ffde80ff644de53592da412cc3300b4d63386
semlex <picasso.semenov.spb@gmail.com>
2025-03-23 07:33:07 +0300

    add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,16 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-plugin-prettier'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -16,9 +20,11 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'prettier': prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "prettier/prettier": ["warn"],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
