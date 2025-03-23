71ca9efa126a0ce584c4cd4d55bf7aafbdf43881
semlex <picasso.semenov.spb@gmail.com>
2025-03-23 22:43:21 +0300

    refactor: rename figure slice

--- src/entities/figure/index.ts
+++ src/entities/figure/index.ts
@@ -1,4 +1,4 @@
 export { default as FigureShape } from './ui/figure-shape';
-export { default as figureReducer } from './model/figureSlice';
-export { addFigure, setSelectedFigure, updateSelectedFigure } from './model/figureSlice';
+export { default as figureReducer } from './model/figure.slice';
+export { addFigure, setSelectedFigure, updateSelectedFigure } from './model/figure.slice';
 export type { Figure } from './model/types';
