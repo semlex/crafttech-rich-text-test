dccb1de868bde55512560c90b9e494de81b95581
semlex <picasso.semenov.spb@gmail.com>
2025-03-23 08:11:27 +0300

    gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/crafttech-rich-text-test/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
