09a8caeb4e08b474d1fa3029c4c1a9d5762145ff
semlex <picasso.semenov.spb@gmail.com>
2025-03-23 22:41:16 +0300

    refactor: rename shared hooks

--- src/shared/libs/index.ts
+++ src/shared/libs/index.ts
@@ -1,2 +1,2 @@
-export { default as useAppDispatch } from './hooks/useAppDispatch';
-export { default as useAppSelector } from './hooks/useAppSelector';
+export { default as useAppDispatch } from './hooks/use-app-dispatch';
+export { default as useAppSelector } from './hooks/use-app-selector';
